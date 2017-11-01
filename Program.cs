9ec308078302952124842404b861b1bb12502da8
BRUNO HENRIQUE AFONSO <44063076806@jpmorgan.sp>
2017-11-01 21:28:51 -0200

    all

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using utilidades;
+
+namespace terminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            documento doc = new documento(); 
+            System.Console.WriteLine(doc.validar_documento());
+                       
+        }
+    }
+}
